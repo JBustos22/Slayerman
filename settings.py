cf6da6dbb06e7a3fea1d025bdf26a6023f72db31
JBustos22 <jorrgebustos@gmail.com>
2020-09-13 22:30:54 -0400

    shit

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import keyring
 
 DB_PASSWORD = keyring.get_password('db_password', 'postgres')
-CLIENT_TOKEN = 'NzUxNTgwNDk2ODY5MTk1ODc3.X1LJ4g.Gmm8W5G3_fF78-joWjWwhym1ag8'
+CLIENT_TOKEN = keyring.get_password('bot_token', 'dfbot')
