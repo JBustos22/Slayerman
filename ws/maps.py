d00feae76966cf683e2caa48db979dc619aa2868
Donaldduck8 <donaldgit@yandex.com>
2020-09-25 12:01:25 +0200

    Take query-based approach to args in !random

--- ws/maps.py
+++ ws/maps.py
@@ -6,145 +6,93 @@ from sqlalchemy import create_engine
 
 
 def get_random_map(modes=[]):
-    retry_limit = 300
-    LONG_RECORDS = None
-
     db = create_engine(CONN_STRING)
 
     with db.connect() as conn:
-        for retry_counter in range(0, retry_limit):
-            # Get a random map
-            if "long" in modes:
-                if not LONG_RECORDS:
-                    # Get a list of 1st place records that are longer than 3 minutes
-                    # Choose a random record from this list, and present that record's map
-                    select_statement = "SELECT *" \
-                                        "FROM mdd_records_ranked " \
-                                        "WHERE player_pos = 1" \
-                                        "AND time_seconds > 180.0"
+        QUERY_PARAMS = []
 
-                    LONG_RECORDS = conn.execute(select_statement).fetchall()
+        if "slick" in modes:
+            QUERY_PARAMS.append("func_slick_flg = B'1'")
+        if "weapon" in modes:
+            QUERY_PARAMS.append("(weap_gl_flg = B'1' OR weap_rl_flg = B'1' OR weap_pg_flg = B'1' OR weap_bf_flg = B'1')")
+        if "strafe" in modes:
+            QUERY_PARAMS.append("(weap_gl_flg = B'0' AND weap_rl_flg = B'0' AND weap_pg_flg = B'0' AND weap_bf_flg = B'0')")
+        if "long" in modes:
+            # TODO: How to make sure that all physics are long?
+            QUERY_PARAMS.append("map_nm IN (SELECT DISTINCT map_name FROM mdd_records_ranked WHERE player_pos = 1 AND time_seconds > 150.0 AND physics = 'cpm-run')")
+        if "good" in modes:
+            # Blacklist
+            blacklist = [
+                "nice",
+                "igoodmap",
+                "moko",
+                "marvin",
+                "gvn",
+                "baulo",
+                "govno",
+                "wesp",
+                "ass",
+                "fag",
+                "shit"
+            ]
 
-                offset = randint(0, len(LONG_RECORDS) - 1)
-                record_r = LONG_RECORDS[offset]
+            blacklist_s = " AND ".join(f"map_nm NOT LIKE '%%{x}%%'" for x in blacklist)
 
-                map_name = record_r.map_name
+            # 50 Records (caveat: total_times is per physics, so maybe a lower value of 35-40 is better here for CPM only)
+            num_records_s = "map_nm IN (SELECT DISTINCT map_name FROM mdd_records_ranked WHERE total_times > 40)"
 
-                # Get map details
-                # A problem can occur here where mdd_records_ranked contains a map,
-                # but ws_maps doesn't.
-                select_statement = "SELECT * " \
-                                    "FROM ws_maps " \
-                                    "WHERE map_nm = %s " \
-                                    "LIMIT 1"
-                replace_vars = (map_name)
+            # Minimum time of 10 seconds
+            min_time_s = "map_nm NOT IN (SELECT DISTINCT map_name FROM mdd_records_ranked WHERE player_pos = 1 AND time_seconds < 10.0)"
 
-                result_set = conn.execute(select_statement, replace_vars)
-                r = result_set.first()
-            else:
-                num_maps = 9251
-                offset = randint(1, num_maps)
+            # TODO Maybe use INTERSECT for these
 
-                # Read
-                select_statement = "SELECT map_nm, author, map_desc, rel_dt, phys_cd_str, func_cd_str, weap_cd_str, " \
-                                   "item_cd_str, func_cd_str " \
-                                   "FROM ws_maps " \
-                                   f"LIMIT 1 OFFSET {offset}"
-                result_set = conn.execute(select_statement)
-                r = result_set.first()
+            QUERY_PARAMS.append(blacklist_s)
+            QUERY_PARAMS.append(num_records_s)
+            QUERY_PARAMS.append(min_time_s)
 
-            if r == None:
-                # DB issues, maybe incomplete ws_maps DB
-                # My scraper didn't look for multiple .defi files per .pk3
-                continue
+        if len(QUERY_PARAMS) > 0:
+            QUERY_WHERE = "WHERE " + " AND ".join(QUERY_PARAMS)
 
-            # Consider all selection modes
-            failed = False
+            select_statement = "SELECT map_nm, author, map_desc, rel_dt, phys_cd_str, func_cd_str, weap_cd_str, " \
+                               "item_cd_str, func_cd_str " \
+                               "FROM ws_maps " \
+                               + QUERY_WHERE
 
-            for mode in modes:
-                if mode == "good":
-                    # Blacklist terms
-                    blacklist = [
-                        "nice",
-                        "igoodmap",
-                        "moko",
-                        "marvin",
-                        "gvn",
-                        "baulo",
-                        "govno",
-                        "wesp",
-                        "ass",
-                        "fag",
-                        "shit"
-                    ]
+            result_set = conn.execute(select_statement)
+            result_rows = result_set.fetchall()
 
-                    if any(x in r.map_nm for x in blacklist):
-                        failed = True
-                        break
+            offset = randint(0, len(result_rows) - 1)
+            r = result_rows[offset]
 
-                    # TODO: Size > 2MB
+        else:
+            num_maps = 9251
+            offset = randint(1, num_maps)
 
-                    # Number of records
-                    select_statement = "SELECT *" \
-                                        "FROM mdd_records_ranked " \
-                                        "WHERE map_name=%s " \
-                                        "ORDER BY time_seconds"
-                    replace_vars = (r.map_nm)
+            # Read
+            select_statement = "SELECT map_nm, author, map_desc, rel_dt, phys_cd_str, func_cd_str, weap_cd_str, " \
+                               "item_cd_str, func_cd_str " \
+                               "FROM ws_maps " \
+                               f"LIMIT 1 OFFSET {offset}"
+            result_set = conn.execute(select_statement)
+            r = result_set.first()
 
-                    result_set = conn.execute(select_statement, replace_vars)
-                    total_records = result_set.rowcount
-
-                    if total_records < 50:
-                        failed = True
-                        break
-
-                    # Minimum time of 10 seconds
-                    fastest_time = result_set.first().time_seconds
-
-                    if fastest_time < 10.0:
-                        failed = True
-                        break
-
-                elif mode == "strafe":
-                    if r.weap_cd_str != None and any(wep in r.weap_cd_str for wep in ["Rocket Launcher", "Plasmagun", "Grenade Launcher", "Big Fucking Gun", "Lightning Gun"]):
-                        failed = True
-                        break
-
-                elif mode == "weapon":
-                    if r.weap_cd_str == None or not any(wep in r.weap_cd_str for wep in ["Rocket Launcher", "Plasmagun", "Grenade Launcher", "Big Fucking Gun", "Lightning Gun"]):
-                        failed = True
-                        break
-
-                elif mode == "slick" or mode == "ice":
-                    if not "Slick" in r.func_cd_str:
-                        failed = True
-                        break
-
-                else:
-                    # No restrictions
-                    pass
-
-            # Select another random map if current map does not fit modes
-            if failed:
-                continue
-
-            map_name = r.map_nm
-            url = f"http://ws.q3df.org/map/{map_name}/"
-            map_data = dict()
-            map_data['name'] = map_name
-            map_data['levelshot_url'] = f"https://ws.q3df.org/images/levelshots/512x384/{map_name}.jpg?fallback=1"
-            map_data['url'] = url
-            fields = dict()
-            fields['Author'] = r.author
-            fields['Description'] = r.map_desc
-            fields['Release Date'] = str(r.rel_dt)
-            fields['Physics'] = r.phys_cd_str
-            opt_fields = {"Weapons": r.weap_cd_str, "Items": r.item_cd_str, "Functions": r.func_cd_str}
-            opt_fields = {key: val for key, val in opt_fields.items() if val != None}  # remove None
-            fields['optional'] = opt_fields
-            map_data['fields'] = fields
-            map_data = {key: val for key, val in map_data.items() if val != None}  # remove None
-            return map_data
+        map_name = r.map_nm
+        url = f"http://ws.q3df.org/map/{map_name}/"
+        map_data = dict()
+        map_data['name'] = map_name
+        map_data['levelshot_url'] = f"https://ws.q3df.org/images/levelshots/512x384/{map_name}.jpg?fallback=1"
+        map_data['url'] = url
+        fields = dict()
+        fields['Author'] = r.author
+        fields['Description'] = r.map_desc
+        fields['Release Date'] = str(r.rel_dt)
+        fields['Physics'] = r.phys_cd_str
+        opt_fields = {"Weapons": r.weap_cd_str, "Items": r.item_cd_str, "Functions": r.func_cd_str}
+        opt_fields = {key: val for key, val in opt_fields.items() if val != None}  # remove None
+        fields['optional'] = opt_fields
+        map_data['fields'] = fields
+        map_data = {key: val for key, val in map_data.items() if val != None}  # remove None
+        return map_data
 
 
 def get_map_data(map_name):
