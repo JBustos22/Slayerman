a8e083c80889c15734d58fd8ce339bfc792ee54b
JBustos22 <jorrgebustos@gmail.com>
2020-10-14 21:08:10 -0400

    cron job and buildscript updates

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose up -d --build
-echo " use docker exec -u postgres df_database bash -c \"sleep 90 && bash /data/restore.sh\ restore database dump"
+echo " use docker exec -u postgres df_database bash -c \"bash /data/restore.sh\" restore database dump"
